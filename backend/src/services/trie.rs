5c08df8e4e9066e4fb6d188af40c7620f1f5ce9f
Sushant <sushant.dhiman9812@gmail.com>
2024-12-25 18:17:34 +0530

    implemented trie

--- backend/src/services/trie.rs
+++ backend/src/services/trie.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 #[derive(Default)]
 pub struct TrieNode {
     children: HashMap<char, TrieNode>,
+    frequency: usize,
     is_end_of_word: bool
 }
 
@@ -13,17 +14,18 @@ pub struct Trie {
 }
 
 impl Trie {
-    pub fn insert(&mut self, word: &str) {
+    fn insert(&mut self, word: &str, frequency: usize) {
         let mut node = &mut self.root;
 
         for ch in word.chars() {
             node = node.children.entry(ch).or_insert_with(TrieNode::default)
         }
 
-        node.is_end_of_word = true
+        node.is_end_of_word = true;
+        node.frequency = frequency;
     }
 
-    pub fn search_prefix(&self, prefix: &str) -> Option<&TrieNode> {
+    fn search_prefix(&self, prefix: &str) -> Option<&TrieNode> {
         let mut node = &self.root;
 
         for ch in prefix.chars() {
@@ -35,5 +37,27 @@ impl Trie {
         }
 
         Some(node)
+    }
+
+    pub fn auto_complete(&self, prefix: &str) -> Vec<String> {
+        let mut suggestions = Vec::new();
+
+        if let Some(node) = self.search_prefix(prefix) {
+            node.collect_words(prefix.to_string(), &mut suggestions);
+        };
+
+        suggestions
+    }
+}
+
+impl TrieNode {
+    fn collect_words(&self, prefix: String, suggestions: &mut Vec<String>) {
+        if self.is_end_of_word {
+            suggestions.push(prefix.clone());
+        }
+
+        for (ch, child) in &self.children {
+            child.collect_words(format!("{}{}", prefix, ch), suggestions);
+        }
     }
 }
