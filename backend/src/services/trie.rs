d7c7944d2eb766fe755bf73f14aab995495dc6bf
Sushant <sushant.dhiman9812@gmail.com>
2024-12-27 07:01:17 +0530

    added function to increase word frequency in trie

--- backend/src/services/trie.rs
+++ backend/src/services/trie.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 
-#[derive(Default)]
+#[derive(Default, Debug, PartialEq, Eq)]
 pub struct TrieNode {
     children: HashMap<char, TrieNode>,
     frequency: usize,
+    word: String,
     is_end_of_word: bool
 }
 
@@ -14,7 +15,7 @@ pub struct Trie {
 }
 
 impl Trie {
-    fn insert(&mut self, word: &str, frequency: usize) {
+    pub fn insert(&mut self, word: &str, frequency: usize) {
         let mut node = &mut self.root;
 
         for ch in word.chars() {
@@ -23,6 +24,7 @@ impl Trie {
 
         node.is_end_of_word = true;
         node.frequency = frequency;
+        node.word = word.to_string();
     }
 
     fn search_prefix(&self, prefix: &str) -> Option<&TrieNode> {
@@ -39,21 +41,36 @@ impl Trie {
         Some(node)
     }
 
-    pub fn auto_complete(&self, prefix: &str) -> Vec<String> {
-        let mut suggestions = Vec::new();
+    pub fn auto_complete(&self, prefix: &str) -> Vec<&TrieNode> {
+        let mut suggestions_node = Vec::new();
 
         if let Some(node) = self.search_prefix(prefix) {
-            node.collect_words(prefix.to_string(), &mut suggestions);
+            node.collect_words(prefix.to_string(), &mut suggestions_node);
         };
 
-        suggestions
+
+        suggestions_node.sort_by(|a, b| b.frequency.cmp(&a.frequency));
+
+        suggestions_node
+    }
+
+    pub fn increase_frequency(&mut self, word: &str, incement_size: usize) {
+        let mut node = &mut self.root;
+
+        for ch in word.chars() {
+            node = node.children.entry(ch).or_insert_with(TrieNode::default)
+        }
+
+        if node.is_end_of_word {
+            node.frequency += incement_size
+        }
     }
 }
 
 impl TrieNode {
-    fn collect_words(&self, prefix: String, suggestions: &mut Vec<String>) {
+    fn collect_words<'a>(&'a self, prefix: String, suggestions: &mut Vec<&'a TrieNode>) {
         if self.is_end_of_word {
-            suggestions.push(prefix.clone());
+            suggestions.push(self);
         }
 
         for (ch, child) in &self.children {
